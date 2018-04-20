3478fdc87f309628ff97ed7aaf65804063cb48ab
iin18 <iin18@primalux.com.sg>
2018-04-20 11:07:53 +0800

    update joke

--- src/app/jokelist.component.ts
+++ src/app/jokelist.component.ts
@@ -25,7 +25,7 @@ export class JokeListComponent{
     jokes: Joke[];
     constructor() {
         this.jokes = [
-            new Joke('What did the cheese say when it looked in the mirror?','Hello-Me (Halloumi)'),
+            new Joke('What AAA did the cheese say when it looked in the mirror?','Hello-Me (Halloumi)'),
             new Joke('What kind of cheese do you use to disguise a small horse?','Mask-a-pony (Mascarpone)'),
             new Joke('A kid threw a lump of cheddar at me','I thought \'That\'s not very mature.\''),
             // {

9d36fbf15a98f0b5f7308f3914ceb0fbc4bc2fe3
iin18 <iin18@primalux.com.sg>
2018-04-20 15:46:12 +0800

    routing and redirect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { JokeComponent } from './joke/joke.component';
 import { JokeListComponent } from './joke-list/joke-list.component';
 import { JokeFormComponent } from './joke-form/joke-form.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
 
 
+const appRoutes: Routes=[
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'jokes', component: JokeListComponent 
+  },
+  {
+    path: 'joke-list', redirectTo: 'jokes'
+  },
+  {
+    path: '**', component: PageNotfoundComponent
+  },
+ 
+];
 //JokeComponent1,
 @NgModule({
   declarations: [
@@ -15,13 +32,18 @@ import { JokeFormComponent } from './joke-form/joke-form.component';
     JokeComponent,
     JokeListComponent,
     JokeFormComponent,
+    HomeComponent,
+    PageNotfoundComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes,),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
   //bootstrap: [JokeComponent]
   //bootstrap: [JokeListComponent]
+
+  
 })
 export class AppModule { }
